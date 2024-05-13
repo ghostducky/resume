19c00add0e7d74e5b758cf4402a6775916abd6a2
onnikeuda <raappanao22@gmail.com>
2024-05-13 10:43:18 +0300

    renamed page content files

---
+++ src/P-content/ContactPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style.css';
+
+function ContactPage() {
+  return (
+    <div>
+      <img src='./images/github-logo-filtered.png' alt='github logo' width="25px" height="25px"/>
+    </div>
+  );
+}
+
+export default ContactPage;
---
+++ src/P-content/ProfilePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style.css';
+
+function ProfilePage() {
+  return (
+    <div className='profileBg Ptext'>
+      <h1>Onni Raappana</h1>
+      <div className='occupation Ptext'>
+        <p>Software developer</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePage;
