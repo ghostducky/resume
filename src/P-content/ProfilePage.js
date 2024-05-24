76834fb759be41cf3105586fdfb0062016fad6b2
onnikeuda <raappanao22@gmail.com>
2024-05-24 09:01:17 +0300

    renaming stuff

--- src/P-content/ProfilePage.js
+++ src/P-content/ProfilePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import '../style.css';
+import '../css/style.css';
 
 function ProfilePage() {
   return (
     <div className='profileBg Ptext'>
       <h1>Onni Raappana</h1>
-      <div className='occupation Ptext'>
+      <div className='occupation text'>
         <p>Software developer</p>
       </div>
     </div>
   );
 }
-
+/**/
 export default ProfilePage;
