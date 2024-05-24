2c7277e69c5133fb7724c0089136adb7a36a7298
onnikeuda <raappanao22@gmail.com>
2024-05-24 09:04:04 +0300

    added pictures
    could've been shorter but whatever

--- src/P-content/ContactPage.js
+++ src/P-content/ContactPage.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import '../style.css';
+import React, { useState } from 'react';
+import '../css/style.css';
 
 function ContactPage() {
+  const [hoveredImage, setHoveredImage] = useState(null);
   return (
-    <div>
-      <img src='./images/github-logo-filtered.png' alt='github logo' width="25px" height="25px"/>
+    <div className='img-container'>
+      <img
+        className='gihub-logo'
+        src={hoveredImage === 'github' ? '/images/github-logo.png' : '/images/github-logo-filtered.png'}
+        alt="Github Logo"
+        width="auto"
+        height="25px"
+        onMouseEnter={() => setHoveredImage('github')}
+        onMouseLeave={() => setHoveredImage(null)}
+      />
+      <img
+        className='linkedin-logo'
+        src={hoveredImage === 'linkedin' ? '/images/linkedin-logo.png' : '/images/linkedin-logo-filtered.png'}
+        alt='Linkedin Logo'
+        width='auto'
+        height='25px'
+        onMouseEnter={() => setHoveredImage('linkedin')}
+        onMouseLeave={() => setHoveredImage(null)}
+      />
+      <img
+        className='mail-logo'
+        src={hoveredImage === 'mail' ? './images/mail-logo.png' : '/images/mail-logo-filtered.png'}
+        alt='Mail Logo'
+        width='auto'
+        height='25px'
+        onMouseEnter={() => setHoveredImage('mail')}
+        onMouseLeave={() => setHoveredImage(null)}
+      />
     </div>
   );
 }
