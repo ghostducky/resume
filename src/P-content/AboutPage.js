7d4c8cf97bc0f62eeaf2f081c8e0b31e83cb15cc
onnikeuda <raappanao22@gmail.com>
2024-05-24 09:08:07 +0300

    lorem ipsum for about me

--- src/P-content/AboutPage.js
+++ src/P-content/AboutPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import '../style.css';
+import '../css/style.css';
 
 function AboutPage() {
   return (
     <div>
       <h1>About Page</h1>
+        <div className='peepee'>
+        {/* Your info table content here */}
+        <p>Vivamus molestie pulvinar pretium. Nam vel leo pulvinar, ornare sem ut, auctor libero. Aliquam sit amet pharetra ipsum. Sed vestibulum tortor purus, nec hendrerit nisl viverra sit amet. Duis pulvinar dolor eget mattis viverra. Sed bibendum ut nulla et molestie. Suspendisse mollis sollicitudin venenatis.</p>
+      </div>
     </div>
   );
 }
-
 export default AboutPage;
