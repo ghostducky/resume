ade439fd8551a1e7b4f3631e4214ad31338cbca4
onnikeuda <raappanao22@gmail.com>
2024-05-24 09:07:20 +0300

    small updates
    import and main page

---
+++ src/main.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import './css/style.css';
+import ProfilePage from './P-content/ProfilePage';
+import ContactPage from './P-content/ContactPage';
+//import AboutPage from './P-content/AboutPage';
+
+
+
+function App() {
+  return (
+    <div className="main">
+      <ProfilePage />
+      <ContactPage />
+    </div>
+  );
+}
+export default App;
+
+
+// UNTIL I GET ONE DONE
+/*
+
+
+<AboutPage />
+
+
+*/
